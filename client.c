96405524d50b8502f206198651a8075d3151280d
evans <munene.evansk@gmail.com>
2016-08-12 10:44:09 -0500

    done: options menu

--- client.c
+++ client.c
@@ -22,7 +22,7 @@ void Connect(int port)
 {
     struct sockaddr_in si_other;
     int s, i, slen=sizeof(si_other);
-    int peers[10] , peercount , found ;
+    int peers[10], finalpeers[10] , peercount , found ;
     char buf[BUFLEN];
     char message[BUFLEN];
 	char* token; 
@@ -82,25 +82,122 @@ void Connect(int port)
 		
 		printf("\n peers");
 		for( i = 0 ; i < peercount ; i ++)
-		{
-			printf("\n %d" , peers[i]);
+		{			
+
+			
+			finalpeers[i] = peers[i] ;
+			printf("\n %d" , finalpeers[i]);
 			}
+			
+		
     
  
     close(s);
     
 }
 
+int Listen(int port)
+{
+	int clients[10], clientcount , found , client;
+    struct sockaddr_in si_me, si_other;
+     
+    int s, i, slen = sizeof(si_other) , recv_len;
+    char buf[BUFLEN] , outbuffer[256] , clientport[5];
+     
+    //initiate client count
+    clientcount = 0 ; 
+    found = 0 ;
+     
+    //create a UDP socket
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        die("socket");
+    }
+     
+    // zero out the structure
+    memset((char *) &si_me, 0, sizeof(si_me));
+     
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons(port);
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+     
+    //bind socket to port
+    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
+    {
+        die("bind");
+    }
+     
+    //keep listening for data
+    while(1)
+    {
+        printf("Waiting for data...");
+        fflush(stdout);
+         
+        //try to receive some data, this is a blocking call
+        if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+        {
+            die("recvfrom()");
+        }
+         
+        //print details of the client/peer and the data received
+        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+        printf("Data: %s\n" , buf);
+        
+        sprintf(outbuffer , "acknowledged");      
+  		printf("\n%s" , outbuffer);
+        
+        
+        //reply
+        if (sendto(s, outbuffer, strlen(outbuffer), 0, (struct sockaddr*) &si_other, slen) == -1)
+        {
+            die("sendto()");
+        }
+        
+    }
+ 
+    close(s);
+    return 0;
+}
+
+
+
 int main()
 {
-	int myport ; 
+	int myport , option; 
 	myport = 0 ;
-	printf("please enter your port\n");
-	scanf("%d", &myport);
 	
-	printf("gonna connect");
-	Connect(myport);
-	getchar();
+	printf("please enter your port\n");
+		scanf("%d", &myport);
+	
+	while(1){
+		
+		
+		printf("\n please enter an option");
+		printf("\n 1: Get clients");
+		printf("\n 2: Talk to peer");
+		scanf("%d" , &option);
+		
+		switch(option)
+		{
+			case 1 :
+				Connect(myport);
+			break ;
+			
+			case 2 :
+			break ;
+			
+			case 3:
+			break ;
+			}
+				
+		
+		
+		
+		
+		
+		
+	
+	}
 	
 	return 0 ;
 	}
